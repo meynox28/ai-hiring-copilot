4e7b70e4ce76259a1702fc2ba22566c5d4188c3f
meynox28 <honmayeesp@gmail.com>
2026-03-22 18:22:55 +0000

    Day 2 - started learning models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+class Job(models.Model):
+    title=models.CharField(max_length=200)
+    descrip=models.TextField()
+    numbs=models.IntegerField()
+    Date=models.DateField()
+    =models.BooleanField()
 # Create your models here.
